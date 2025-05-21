da1bf081aa3cf9537b29a60425eb47906e541c74
Spark <legion20220808@gmail.com>
2025-05-22 02:22:46 +0800

    modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  website: "https://xqark.github.io/", // replace this with your deployed domain
+  author: "xqark",
+  profile: "https://xqark.github.io/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
